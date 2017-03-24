fa7004b54dd7084f2b63fe6030c231ef43bda161
SomayaB <somaya.bounouar@gmail.com>
2017-03-24 11:40:10 -0700

    Complete 12 out of 13 Day4 exercises

---
+++ sql/01-create-table-student.sql
@@ -0,0 +1,1 @@
+CREATE TABLE student (id serial PRIMARY KEY, name TEXT, grade INTEGER);
---
+++ sql/06-load-table-like.sql
@@ -0,0 +1,5 @@
+\c sql-exercises
+
+ COPY student_like
+ FROM '/Users/Somaya/Desktop/LGProjects/sql-exercises/data/likes.csv' DELIMITER ',' CSV HEADER;
+ SELECT * FROM student_like;
